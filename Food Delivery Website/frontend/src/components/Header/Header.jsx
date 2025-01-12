9b858b57293b1f7d7e72da82371f30dd247a3465
Nikhil Khandelwal <khandelwalnikhil006@gmail.com>
2025-01-12 16:56:23 +0530

    Food Delivery Website

---
+++ Food Delivery Website/frontend/src/components/Header/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Header.css'
+const Header = () => {
+  return (
+    <div className='header'>
+      <div className="header-contents">
+        <h2>Order your favorite food here</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae ipsam provident atque cupiditate quaerat doloremque modi aut recusandae. </p>
+        <button>View Menu</button>
+      </div>
+    </div>
+  )
+}
+
+export default Header
