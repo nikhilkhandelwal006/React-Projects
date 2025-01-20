4fb3ab0c99bb2aa536132cdf1143c17e008f7d3f
Nikhil Khandelwal <khandelwalnikhil006@gmail.com>
2025-01-20 19:33:05 +0530

    AI-Image Generator

---
+++ AI-Image Generator/react-app/src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+import ImageGenerator from './components/ImageGenerator/ImageGenerator'
+
+function App() {
+  
+
+  return (
+    <>
+     <ImageGenerator/>
+        
+    </>
+  )
+}
+
+export default App
---
+++ AI-Image Generator/react-app/src/components/ImageGenerator/ImageGenerator.jsx
@@ -0,0 +1,53 @@
+import React, { useState,useRef } from 'react'
+import './ImageGenerator.css'
+import default_image from '../../assets/default_image.svg'
+
+const ImageGenerator = () => {
+
+    const[image_url,setImage_url]=useState("/")
+    let inputRef=useRef(null)
+
+    const imageGenerator=async()=>{
+        if(inputRef.current.value===""){
+            return 0;
+        }
+        const response=await fetch(" https://api.openai.com/v1/images/generations",{
+            method:"POST",
+            headers:{
+                "Content-Type":"application/json",
+                Authorization:
+                "Bearer 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",
+                "User-Agent":"Chrome",
+            },
+            body:JSON.stringify({
+                prompt:`${inputRef.current.value}`,
+                n:1,
+                size:"512x512",
+            }),
+        });
+        let data=await response.json()
+        console.log(data)
+        let data_array=data.data;
+        setImage_url(data_array[0].url)
+    }
+    
+    
+  return (
+    <div className='ai-image-generator'>
+    <div className="header">Ai image <span>generator</span></div>  
+
+    <div className='img-loading'>
+        <div className="image">
+            <img src={image_url==="/"?default_image:image_url}  />
+
+        </div>
+    </div>
+     <div className="search-box">
+        <input type="text" ref={inputRef} className='search-input' placeholder='Describe What you want to see' />
+        <div className="generate-btn" onClick={()=>{imageGenerator()}}>Generate</div>
+     </div>
+    </div>
+  )
+}
+
+export default ImageGenerator
