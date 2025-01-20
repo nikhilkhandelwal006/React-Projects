c1b9a0dbdd4f45b3b364dd7e573227d20382b331
Nikhil Khandelwal <khandelwalnikhil006@gmail.com>
2025-01-20 19:39:17 +0530

    AI-Image Generator

--- AI-Image Generator/react-app/src/components/ImageGenerator/ImageGenerator.jsx
+++ AI-Image Generator/react-app/src/components/ImageGenerator/ImageGenerator.jsx
@@ -7,27 +7,8 @@ const ImageGenerator = () => {
     const[image_url,setImage_url]=useState("/")
     let inputRef=useRef(null)
 
-    const imageGenerator=async()=>{
-        if(inputRef.current.value===""){
-            return 0;
-        }
-        const response=await fetch(" https://api.openai.com/v1/images/generations",{
-            method:"POST",
-            headers:{
-                "Content-Type":"application/json",
-             
-            },
-            body:JSON.stringify({
-                prompt:`${inputRef.current.value}`,
-                n:1,
-                size:"512x512",
-            }),
-        });
-        let data=await response.json()
-        console.log(data)
-        let data_array=data.data;
-        setImage_url(data_array[0].url)
-    }
+    
+    
     
     
   return (
